bb3ddd48efc59cd25ff39fb0ffa2e04c1089dec5
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-07 03:00:39 +0200

    Add CGP (Collectd Graph Panel)

---
+++ config.local.php
@@ -0,0 +1,11 @@
+<?php
+$CONFIG['cat']['testnet'] = '/^node\d+\.testnet\.rchain-dev\.tk$/'
+$CONFIG['overview'] = array('load', 'cpu', 'memory')
+$CONFIG['overview_filter']['cpu'] = array('pi' => 'average');
+$CONFIG['time_range']['default'] = 7200;
+$CONFIG['showload'] = true;
+$CONFIG['showmem'] = true;
+$CONFIG['showtime'] = true;
+$CONFIG['graph_type'] = 'hybrid';
+$CONFIG['graph_smooth'] = true;
+$CONFIG['graph_minmax'] = true;
